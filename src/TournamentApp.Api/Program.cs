3fc86fe50f90634c31e5e2eba05058f09833966b
Eamonn Connolly <eamonn@evolving.technology>
2025-12-07 21:37:50 +0400

    turn on swagger

--- src/TournamentApp.Api/Program.cs
+++ src/TournamentApp.Api/Program.cs
@@ -64,11 +64,9 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Enable Swagger in all environments for API documentation
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowGitHubPages");
